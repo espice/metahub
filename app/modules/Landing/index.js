b15f74080019ad8fe11969ff8451bd8a6b8f6dc1
Daksh <dakshshrivastava2007@gmail.com>
2022-07-30 13:27:14 +0530

    Budhee-Heen Thanu Jannikay

--- app/modules/Landing/index.js
+++ app/modules/Landing/index.js
@@ -1,10 +1,34 @@
+import Image from 'next/image';
+import PrimaryButton from '../../components/Button/Primary';
+import TextButton from '../../components/Button/Text';
 import styles from './index.module.scss';
 
 export default function Content() {
   return (
     <div className={styles.content}>
-      <h1>Landing Page</h1>
-      <p>This is the landing page content.</p>
+      <div className={styles.left}>
+        <div>
+          <h1>
+            Welcome to <span className={styles.blue}>App Name</span>.
+          </h1>
+          <h1>
+            Your gateway to the <span className={styles.blue}>Metaverse.</span>
+          </h1>
+        </div>
+        <div className={styles.buttons}>
+          <PrimaryButton>
+            <div className={styles.apart}>
+              <div>Get Started</div> <div>→</div>
+            </div>
+          </PrimaryButton>
+          <TextButton>
+            <div className={styles.something}>
+              <div>Explore Features</div> <div>→</div>
+            </div>
+          </TextButton>
+        </div>
+      </div>
+      <img src="https://cdn.discordapp.com/attachments/778128218128515074/1002715143379095763/unknown.png" />
     </div>
   );
 }
